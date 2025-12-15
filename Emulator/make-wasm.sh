43e0ef066767457cab2ba743b89eb85cde008b39
smal <6433336+smaldragon@users.noreply.github.com>
2025-12-15 12:28:38 +0000

    switch cpu to submodule

--- Emulator/make-wasm.sh
+++ Emulator/make-wasm.sh
@@ -1,3 +1,7 @@
-cp -R ../MBios roms
+ln -s ../MBios roms
+rm w6502.c
+ln -s ../kit6502/wdc65c02.c w6502.c
+
 emcc -O3 system.c -s WASM=1 -s USE_SDL=2 --preload-file roms -o build-wasm/index.js
+
 rm -r roms
--- Emulator/make.sh
+++ Emulator/make.sh
@@ -1,6 +1,8 @@
 rm -r build
 mkdir build
-cp -R ../MBios build/roms
+ln -s ../../MBios build/roms
+rm w6502.c
+ln -s ../kit6502/wdc65c02.c w6502.c
 
 if gcc system.c -Llib -lSDL2 -lSDL2_image; then
     mv a.out build/microemu
